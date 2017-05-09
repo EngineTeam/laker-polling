2ab4d668ff7180f79b97ed63fa00febb50e45086
zsabin <zsabin@gmail.com>
2017-05-08 22:22:31 -0400

    Fixed bug with not being able to view help page from landing page

--- grails-app/controllers/interceptors/AuthCheckInterceptor.groovy
+++ grails-app/controllers/interceptors/AuthCheckInterceptor.groovy
@@ -6,6 +6,7 @@ class AuthCheckInterceptor {
     AuthCheckInterceptor() {
         matchAll()
                 .excludes(controller: 'application', action: 'landing')
+                .excludes(controller: 'application', action: 'landingPageHelpView')
                 .excludes(controller: 'auth', action: 'auth')
                 .excludes(controller: 'auth', action: 'logout')
                 .excludes(controller: 'course')
